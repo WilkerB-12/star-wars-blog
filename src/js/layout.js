ee8cd8ed6ee02ee8731b17316f6868ab47659f84
Wilker Blanco <wilkerblanco18@gmail.com>
2022-10-10 01:01:36 -0400

    eliminada la variable basename

--- src/js/layout.js
+++ src/js/layout.js
@@ -15,11 +15,11 @@ const Layout = ({item}) => {
 
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
 	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-	const basename = process.env.PUBLIC_URL || "";
+	//const basename = process.env.PUBLIC_URL || "";
 
 	return (
 		<div>
-			<BrowserRouter basename={basename}>
+			<BrowserRouter /*basename={basename}*/>
 				<ScrollToTop>
 					<Navbar />
 					<Switch>
